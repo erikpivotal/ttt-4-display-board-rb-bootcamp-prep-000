25ca0b1b4111011c2412a47426bb5e84b2c28a76
Erik Rodriguez <erikrdgz@hotmail.com>
2018-04-13 20:24:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,5 @@ def display_board board
   cell = "   "
   divider = "\n" + "-----------" + "\n"
   row = [cell + "|" + cell + "|" + cell, divider, cell + "|" + cell + "|" + cell, divider , cell + "|" + cell + "|" + cell ]
-  game_board = row + divider + row + divider + row
-  puts game_board
+  puts row
 end
