11bb7700063af6be3867aa0dddc2b52b7a43c025
Erik Rodriguez <erikrdgz@hotmail.com>
2018-04-13 20:33:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,6 @@
 # out the current state.
 def display_board board
   divider = "\n" + "-----------" + "\n"
-  row = ["#{board[0]}" + "|" + "#{board[1]}" + "|" + "#{board[2]}", divider, "#{board[3]}" + "|" + "board[4]" + "|" + board[5], divider , "#{board[6]}" + "|" + "#{board[7]}" + "|" + "#{board[8]}" ]
+  row = ["#{board[0]}" + "|" + "#{board[1]}" + "|" + "#{board[2]}", divider, "#{board[3]}" + "|" + "#{board[4]}" + "|" + #{board[5]}, divider , "#{board[6]}" + "|" + "#{board[7]}" + "|" + "#{board[8]}" ]
   puts row
 end
